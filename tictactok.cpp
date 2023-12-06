37569237e72be0f540d50f1645e37857f5f30ce3
amirhosseintl04 <13830725amir@gmail.com>
2023-12-06 20:38:17 +0330

    Co-authored-by: parzallel <parzallel@users.noreply.github.com> Co-authored-by: Mehrab123dehghan <Mehrab123dehghan@users.noreply.github.com>

--- tictactok.cpp
+++ tictactok.cpp
@@ -2,9 +2,81 @@
 #define RED "\033[31m"
 #define LBLUE "\033[94m"
 #define RESET "\033[0m"
+/*
+return 1 == x won ;
+return 2 == o won ;
+retrurn 0 == no one won  ;
+ */
+int Win(std::string matrix[][3])
+{
+
+    for (int i = 0; i < 3; i++)
+    {
+
+        if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2])
+        {
+            if (matrix[i][0] == "x")
+            {
+                return 1;
+            }
+            else if (matrix[i][0] == "o")
+            {
+                return 2;
+            }
+        }
+
+        if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i])
+        {
+            if (matrix[i][0] == "x")
+            {
+                return 1;
+            }
+            else if (matrix[i][0] == "o")
+            {
+                return 2;
+            }
+        }
+    }
+    if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2])
+    {
+        if (matrix[0][0] == "x")
+        {
+            return 1;
+        }
+        else if (matrix[0][0] == "o")
+        {
+            return 2;
+        }
+    }
+    if (matrix[2][0] == matrix[1][1] && matrix[1][1] == matrix[0][2])
+    {
+        if (matrix[1][1] == "x")
+        {
+            return 1;
+        }
+        else if (matrix[1][1] == "o")
+        {
+            return 2;
+        }
+    }
+
+    return 0;
+}
+void drawBoard(std::string matrix[][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << matrix[i][j] << "  ";
+        }
+        std::cout << "\n";
+    }
+}
 int main()
 {
-    system("cls");
+    system("cls"); // for convinience
+    int victory; //checks for the wo
     std::string mat00[3][3];
     for (int i = 0; i < 3; i++)
     {
@@ -15,45 +87,60 @@ int main()
     }
     int i, j;
 
-    for (int k = 0; k < 9 ; k++)
+    for (int k = 0; k < 9; k++)
     {
-        if (k % 2 == 0){
+        drawBoard(mat00);
+        if (k % 2 == 0)
+        {
+
             do
             {
-                std::cout <<RED<< "ij" << k+1 <<RESET<< std::endl;
+                std::cout << RED << "ij" << k + 1 << RESET << " ";
                 std::cin >> i >> j;
-            } while ((i>2 || i<0|| j<0|| j>2)&&mat00[i][j] != "0");
+
+            } while ((i > 2 || i < 0 || j < 0 || j > 2) && mat00[i][j] != "0" || mat00[i][j] == "o" || mat00[i][j] == "x");
+            mat00[i][j] = "o";
         }
-        else{
+        else
+        {
+
             do
             {
-                std::cout <<LBLUE<< "ij" << k+1 <<RESET<< std::endl;
+
+                std::cout << LBLUE << "ij" << k + 1 << RESET << " ";
                 std::cin >> i >> j;
-            } while ((i>2 || i<0|| j<0|| j>2)&&mat00[i][j] != "0");
-        }
-        if(k%2==0){
-            do{
-                std::cout<<"xORo";
-                std::cin >> mat00[i][j];
-            } while (mat00[i][j]!="o"); 
-        }else{
-            do{
-                std::cout<<"xORo";
-                std::cin >> mat00[i][j];
-            } while (mat00[i][j]!="x");
+
+            } while ((i > 2 || i < 0 || j < 0 || j > 2) && mat00[i][j] != "0" || mat00[i][j] == "o" || mat00[i][j] == "x");
+
+            mat00[i][j] = "x";
         }
         system("cls");
-    }
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
+        victory = Win(mat00);
+
+        if (victory == 1)
         {
-            std::cout << mat00[i][j]<<"  ";
+            std::cout << "X won";
+            return 0;
         }
-        std::cout << "\n";
+        else if (victory == 2)
+        {
+            std::cout << "O won";
+            return 0;
+        }
+
     }
+    std::cout<<"DRAW";
     return 0;
 }
 
 /*از k بزرگ تر 5 شروع کن شرط ویکتوری را چک کردن*/
+// O RED zooj ... X LBLUE fard
+// ایکس میتونه جای او را بگیره BUG
+
+// شرط برنده شدن مساوی بودن این خونه پایینیا
+// player mitone X ya O bashe
+//[i][0]== player && [i][1] == player && [i][2] == player
+//[0][i]== player && [1][i] == player && [2][i] == player
+//[0][0]== player && [1][1] == player && [2][2] == player
+//[0][2]== player && [1][1] == player && [2][0] == player
