24476ae4519a3b42acdfe35ee52ae2185fafac89
amirhosseintl04 <13830725amir@gmail.com>
2023-12-13 00:44:57 +0330

    last

--- tictactok.cpp
+++ tictactok.cpp
@@ -5,23 +5,9 @@
 #define LBLUE "\033[94m"
 #define RESET "\033[0m"
 
-// #define KEY_UP 72
-// #define KEY_DOWN 80
-// #define KEY_LEFT 75
-// #define KEY_RIGHT 77
 #define Enter 13
 #define Back 8
 #define Space 32
-
-
-// struct Student {
-//     int age;
-//     std::string name;
-
-//     void tellName() {
-//         std::cout << name;
-//     }
-// };
 
     /**
     * return 1 == x won
@@ -32,65 +18,45 @@
     */
 int Win(std::string matrix[][3])
 {
-    // Student haas(20, "MOhsen");
-    // haas.tellName();
 
-    for (int i = 0; i < 3; i++)
-    {
-        if (matrix[i][0] != "." && matrix[i][1] != "." && matrix[i][2] != ".")
-        {
-            if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2])
-            {
+    for (int i = 0; i < 3; i++){
+        if (matrix[i][0] != "." && matrix[i][1] != "." && matrix[i][2] != "."){
+            if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2]){
 
-                if (matrix[i][0] == "x")
-                {
+                if (matrix[i][0] == "x"){
                     return 1;
-                }
-                else if (matrix[i][0] == "o")
-                {
+                }else if (matrix[i][0] == "o"){
                     return 2;
                 }
             }
         }
-        if (matrix[0][i] != "." && matrix[1][i] != "." && matrix[2][i] != ".")
-        {
-            if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i])
-            {
-                if (matrix[0][i] == "x")
-                {
+        if (matrix[0][i] != "." && matrix[1][i] != "." && matrix[2][i] != "."){
+            if (matrix[0][i] == matrix[1][i] && matrix[1][i] == matrix[2][i]){
+                
+                if (matrix[0][i] == "x"){
                     return 1;
-                }
-                else if (matrix[0][i] == "o")
-                {
+                }else if (matrix[0][i] == "o"){
                     return 2;
                 }
             }
         }
     }
-    if (matrix[0][0] != "." && matrix[1][1] != "." && matrix[2][2] != ".")
-    {
-        if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2])
-        {
-            if (matrix[0][0] == "x")
-            {
+    if (matrix[0][0] != "." && matrix[1][1] != "." && matrix[2][2] != "."){
+        if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]){
+            if (matrix[0][0] == "x"){
                 return 1;
             }
-            else if (matrix[0][0] == "o")
-            {
+            else if (matrix[0][0] == "o"){
                 return 2;
             }
         }
     }
-    if (matrix[2][0] != "." && matrix[1][1] != "." && matrix[0][2] != ".")
-    {
-        if (matrix[2][0] == matrix[1][1] && matrix[1][1] == matrix[0][2])
-        {
-            if (matrix[1][1] == "x")
-            {
+    if (matrix[2][0] != "." && matrix[1][1] != "." && matrix[0][2] != "."){
+        if (matrix[2][0] == matrix[1][1] && matrix[1][1] == matrix[0][2]){
+
+            if (matrix[1][1] == "x"){
                 return 1;
-            }
-            else if (matrix[1][1] == "o")
-            {
+            }else if (matrix[1][1] == "o"){
                 return 2;
             }
         }
@@ -98,20 +64,16 @@ int Win(std::string matrix[][3])
 
     return 0;
 }
-
-void drawBoard(std::string matrix[][3])
-{
+void drawBoard(std::string matrix[][3]){
     std::cout << "+---+---+---+" << std::endl;
 
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < 3; i++){
         // std::cout << i + 1 << " ";
         std::cout << "|";
-        for (int j = 0; j < 3; j++)
-        {
+        for (int j = 0; j < 3; j++){
+
             std::cout << " " << matrix[i][j] << " ";
-            if (j < 2)
-                std::cout << "|";
+            if (j < 2) std::cout << "|";
         }
         std::cout << "|";
         std::cout << std::endl;
@@ -122,101 +84,63 @@ void drawBoard(std::string matrix[][3])
     std::cout << std::endl;
 }
 
-void initialize(std::string mat[3][3])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
+void initialize(std::string mat[3][3]){
+    
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
             mat[i][j] = ".";
         }
     }
 }
 
-void turn(std::string mat[3][3], int k, bool move, int i, int j)
-{
-    if (mat[i][j] == ".")
-    {
+void turn(std::string ogMat[3][3],std::string mat[3][3], int& k, bool& move, int i, int j){
+
+    if (mat[i][j] == "."){
         system("cls");
-        if (k % 2 == 0)
-        {
+        if (k % 2 == 0){
             mat[i][j] = "o";
-        }
-        else
-        {
+        }else{
             mat[i][j] = "x";
         }
         drawBoard(mat);
+        std::cout << "\nOriginal" << std::endl ;
+        drawBoard(ogMat);
         move = true;
     }
 }
-void Switch(int k, bool move, std::string mat[3][3], bool enter,int i,int j)
-{
-    //bool moved=move;
-    do
-    {
-        switch (getch())
-        {
-        case '1':
-            i = 2; j = 0;
-            turn(mat, k, move, i, j);
-            break;
+void Switch(std::string ogMat[3][3],int& k, bool& move, std::string mat[3][3], bool& enter,int& i,int j){
+    
+    do{
 
-        case '2':
-            i = 2; j = 1;
-            turn(mat, k, move, i, j);
-            break;
+        switch (getch()){
 
-        case '3':
-            i = 2; j = 2;
-            turn(mat, k, move, i, j);
-            break;
+        case '1': i = 2; j = 0; turn(ogMat,mat, k, move, i, j); break;
+        case '2': i = 2; j = 1; turn(ogMat,mat, k, move, i, j); break;
+        case '3': i = 2; j = 2; turn(ogMat,mat, k, move, i, j); break;
+        case '4': i = 1; j = 0; turn(ogMat,mat, k, move, i, j); break;
+        case '5': i = 1; j = 1; turn(ogMat,mat, k, move, i, j); break;
+        case '6': i = 1; j = 2; turn(ogMat,mat, k, move, i, j); break;
+        case '7': i = 0; j = 0; turn(ogMat,mat, k, move, i, j); break;
+        case '8': i = 0; j = 1; turn(ogMat,mat, k, move, i, j); break;
+        case '9': i = 0; j = 2; turn(ogMat,mat, k, move, i, j); break;
 
-        case '4':
-            i = 1; j = 0;
-            turn(mat, k, move, i, j);
-            break;
-
-        case '5':
-            i = 1; j = 1;
-            turn(mat, k, move, i, j);
-            break;
-        case '6':
-            i = 1; j = 2;
-            turn(mat, k, move, i, j);
-            break;
-
-        case '7':
-            i = 0; j = 0;
-            turn(mat, k, move, i, j);
-            break;
-
-        case '8':
-            i = 0; j = 1;
-            turn(mat, k, move, i, j);
-            break;
-        case '9':
-            i = 0; j = 2;
-            turn(mat, k, move, i, j);
-            break;
-
-        case Space:
+        case Back:
             system("cls");
             mat[i][j] = ".";
             drawBoard(mat);
+            std::cout << "\nOriginal" << std::endl ;
+            drawBoard(ogMat);
             move = false;
             break;
 
         case Enter:
-            if (move)
-             {
+            if (move){
                 enter = false;
-            }
-             else
-             {
+            }else{
                  std::cout << "enteret nashod ";
-             }
+            }
             break;
+            
         default:
             system("cls");
             std::cout << "kossher vared nakon" << std::endl;
@@ -224,41 +148,89 @@ void Switch(int k, bool move, std::string mat[3][3], bool enter,int i,int j)
         }
     } while (enter);
 }
-
-int main()
-{
-    int i,j;
-    system("cls");
-    bool move = false;
-    int victory;
-    std::string mat00[3][3];
-    initialize(mat00);
+void game(std::string ogMat[3][3],std::string mat[3][3] , bool move , int& i ,int& j ,int x,int y){
+    initialize(mat);
+    int victory ;
     for (int k = 0; k < 9; k++)
     {
-
         system("cls");
         bool enter = true;
-        drawBoard(mat00);
-        Switch(k, move, mat00, enter,i,j);
-        victory = Win(mat00);
+        drawBoard(mat);
+        std::cout << "\nOriginal" << std::endl ;
+        drawBoard(ogMat);
+        
+        Switch(ogMat,k, move, mat, enter,i,j);
 
-        if (victory == 1)
-        {
-            std::cout << "X won";
-            return 0;
-        }
-        else if (victory == 2)
-        {
-            std::cout << "O won";
-            return 0;
+        victory = Win(mat);
+        if (victory == 1){
+            //std::cout << "X won";
+            ogMat[x][y]="X";
+            return;
+        }else if (victory == 2){
+            ogMat[x][y]="O";
+            return;
         }
     }
+    ogMat[x][y]="D";
+    //std::cout << "DRAW";
+}
+int main()
+{
+    std::string ogMat[3][3];
+    std::string gameMat[3][3];
+    int i,j,x,y;
+    int vicivici;
+    bool move = false;
+
+    system("cls");
+    initialize(ogMat);
+    std::cout<<"Orginal\n";
+    drawBoard(ogMat);
+    for(int e=0; e<9;e++){
+    switch (getch()){
+
+        case '1': x = 2; y = 0;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '2': x = 2; y = 1;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '3': x = 2; y = 2;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '4': x = 1; y = 0;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '5': x = 1; y = 1;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '6': x = 1; y = 2;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '7': x = 0; y = 0;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '8': x = 0; y = 1;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        case '9': x = 0; y = 2;game(ogMat ,gameMat , move , i , j , x , y); system("cls");std::cout<<"Orginal\n"; drawBoard(ogMat);vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;} break;
+        
+        }
+    }
+           
+    //vicivici = Win(ogMat);if (vicivici == 1){std::cout << "X won";return 0;}else if (vicivici == 2){std::cout << "O won";return 0;}
+
     std::cout << "DRAW";
+    
+    system("pause") ;
     return 0;
 }
 
 /*
-bugs:
-if press any null key turn will expire;
-and big dick bug in changing elements;
+        
+    // for (int k = 0; k < 9; k++)
+    // {
+    //     system("cls");
+    //     bool enter = true;
+    //     drawBoard(mat00);
+    //     Switch(k, move, mat00, enter,i,j);
+
+    
+    //     victory = Win(mat00);
+    //     if (victory == 1){
+
+    //         std::cout << "X won";
+    //         return 0;
+
+    //     }else if (victory == 2){
+
+    //         std::cout << "O won";
+    //         return 0;
+    //     }
+    // }
+    
 */
